c83996761c4be6224f41f503ba157765bb7530fd
Gilang R. Aprianto <gilang.aprianto@mekari.com>
2025-02-20 22:38:40 +0700

    Initialize follower and followed associations in User and Follow models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :sleep_records, dependent: :destroy  
+
+  # As a follower
+  has_many :following, through: :follows, source: :followed
+  has_many :follows, foreign_key: :follower_id, dependent: :destroy
+  
+  # As a followed user
+  has_many :followers, through: :following_users, source: :follower
+  has_many :following_users, foreign_key: :followed_id, class_name: "Follow", dependent: :destroy
 end
