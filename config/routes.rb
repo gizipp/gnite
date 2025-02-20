763c794eb77e493ab3ea9d3ce448498a8896fd59
Gilang R. Aprianto <gilang.aprianto@mekari.com>
2025-02-20 22:16:50 +0700

    Add API routes for sleep_records and follows, including clock_in, clock_out, and following_sleep_records

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :sleep_records, only: [:index] do
+        collection do
+          post :clock_in
+        end
+        member do
+          patch :clock_out
+        end
+      end
+      
+      resources :follows, only: [:create, :destroy] do
+        collection do
+          get :following_sleep_records
+        end
+      end
+    end
+  end
 end
