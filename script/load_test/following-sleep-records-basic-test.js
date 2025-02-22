556181c7f69eccc8f8f029e3b1db90156352f9b0
Gilang R. Aprianto <gilangr.aprianto@gmail.com>
2025-02-23 05:16:50 +0700

    Decrease base perf test to 1m

--- script/load_test/following-sleep-records-basic-test.js
+++ script/load_test/following-sleep-records-basic-test.js
@@ -4,10 +4,10 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 10 }, // Ramp-up to 10 users
-    { duration: '1m', target: 50 },  // Ramp-up to 50 users
-    { duration: '2m', target: 100 },  // Ramp-up to 100 users
-    { duration: '30s', target: 0 },  // Ramp-down to 0 users
+    { duration: '10s', target: 10 }, // Ramp-up to 10 users
+    { duration: '30s', target: 50 },  // Ramp-up to 50 users
+    { duration: '10s', target: 100 },  // Ramp-up to 100 users
+    { duration: '10s', target: 0 },  // Ramp-down to 0 users
   ],
   thresholds: {
     http_req_duration: ['p(95)<500'], // 95% of requests should be below 500ms
